29ccd0dd7f540f02da615ff5a32554696d74d3ac
TranDung <dung2002lca1@gmail.com>
2026-01-13 23:41:50 +0700

    Refactor structure for CI/CD phase 3

--- gateway/mqtt_to_kafka.py
+++ gateway/mqtt_to_kafka.py
@@ -1,13 +1,17 @@
+import os
 import json
 import time
 import paho.mqtt.client as mqtt
 from kafka import KafkaProducer
 
-# Cấu hình
-MQTT_BROKER = "mosquitto"
-MQTT_TOPIC = "sensors/#" 
-KAFKA_BROKER = "kafka:29092"
-KAFKA_TOPIC = "landslide_data"
+# Cấu hình: Ưu tiên lấy từ biến môi trường, nếu không có mới dùng mặc định
+MQTT_BROKER = os.getenv('MQTT_BROKER', 'mosquitto')
+MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
+MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'sensors/#')
+
+# QUAN TRỌNG: Để kết nối Server từ xa
+KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:29092')
+KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'landslide_data')
 
 # Kết nối Kafka (Thử lại cho đến khi thành công)
 producer = None
@@ -38,7 +42,7 @@ client = mqtt.Client()
 client.on_message = on_message
 while True:
     try:
-        client.connect(MQTT_BROKER, 1883, 60)
+        client.connect(MQTT_BROKER, MQTT_PORT, 60)
         client.subscribe(MQTT_TOPIC)
         print(f">>> Bridge: Da ket noi MQTT va dang nghe {MQTT_TOPIC}")
         client.loop_forever()
